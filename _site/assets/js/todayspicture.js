deef2470d2b5f0a7fb3ec659bb5f96f0bc90dba2
brian <brianjbutcher65@gmail.com>
2019-09-28 12:22:10 +0100

    modified:   _includes/head.html 	modified:   _layouts/home.html 	modified:   assets/js/todayspicture.js js linked

--- _site/assets/js/todayspicture.js
+++ _site/assets/js/todayspicture.js
@@ -1,46 +1,5 @@
-// return gamecount
-function gc( txt ){
-  'use strict';
-
-  var count = 0;
-  var lines = txt.split("\n");
-  for (var i = 0, len = lines.length; i < len; i++) {
-    var l = lines[i].trim();
-    if( l.endsWith("1-0") || l.endsWith("0-1") || l.endsWith("1/2-1/2") ){ 
-      count++; 
-    };  // end if
-  };    // end for
-    
-  return count;
-};
-
-
-function todaysgamepictureindex( txt ){
-  return ( Math.floor( Date.now() / 86400000 ) % gc( txt )); 
-}
-
-
-// return a substring with todays game only
-function todaysgame( txt ){
-
-  var todaysgameindex = Math.floor( Date.now() / 86400000 ) % gc( txt );  // id of game in txt
-  var gamecount = 0;
-  var startchari = 0;
-  var endchari = 0;
-  var lines = txt.split(/\r|\n/); // Why does this work without parenthesis?
-  var pgntext = lines.join('\n');
-  
-  // get end position and start position
-  for ( var i = 0, len = lines.length; i < len; i++) {
-    endchari += lines[i].length + 1; // don't forget the '\n'
-    var l = lines[i].trim();
-    if( l.endsWith("1-0") || l.endsWith("0-1") || l.endsWith("1/2-1/2") || l.endsWith("*") ){ 
-      if ( gamecount === todaysgameindex ){ break }; 
-      startchari = endchari;
-      gamecount++;
-    }; 
-  }
-  
-  return pgntext.slice( startchari, endchari ).trim();
+function todayspictureindex( picturecount ){
+  /* alert("todayspictureindex: " + Math.floor( Date.now() / 86400000 ) % picturecount ); */
+  return ( Math.floor( Date.now() / 86400000 ) % picturecount ); 
 }
 
