d1507dbf134b414486cdc9dc939203ded504279c
brian <brianjbutcher65@gmail.com>
2019-09-29 10:37:28 +0100

    modified:   _includes/head.html 	modified:   _layouts/home.html 	new file:   _layouts/homeshell.html 	modified:   assets/js/todayspicture.js development of multiple images

--- _site/assets/js/todayspicture.js
+++ _site/assets/js/todayspicture.js
@@ -1,5 +1,13 @@
-function todayspictureindex( picturecount ){
-  /* alert("todayspictureindex: " + Math.floor( Date.now() / 86400000 ) % picturecount ); */
-  return ( Math.floor( Date.now() / 86400000 ) % picturecount ); 
-}
+function loadfig(){
+  var srcs = srclist.split(',');
+  //alert( "srcs: " + srcs );
+  captions = captionlist.split(',');
+  var index = Math.floor( Date.now() / 86400000 ) % captions.length; 
+  //alert( "array length: " + captions.length );
+  //alert( "index:" + index );
+  //alert( "srcs: " + srcs[index] );
+  //alert( captions[index] );
 
+  $("#img").attr("src", srcs[index]);
+  $("#figcaption").append( captions[index]); 
+}
